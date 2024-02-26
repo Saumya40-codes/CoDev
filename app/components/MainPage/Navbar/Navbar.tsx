b749c644ff529e57d7def9545584d7cd3f6efed1
Saumya <saumyabshah90@gmail.com>
2024-02-26 18:51:36 +0530

    update: added next-auth

--- app/components/MainPage/Navbar/Navbar.tsx
+++ app/components/MainPage/Navbar/Navbar.tsx
@@ -3,8 +3,14 @@ import logo from '../../../../styles/images/logo.png'
 import styles from './navbar.module.css'
 import Link from 'next/link'
 import Auth from '../../Auth/Auth'
+import { getServerSession } from "next-auth";
+import { authConfig } from '@/app/lib/auth/auth'
+import { Avatar, AvatarBadge, AvatarGroup } from '@chakra-ui/react'
 
-const Navbar = () => {
+const Navbar = async() => {
+
+    const session = await getServerSession(authConfig);
+
   return (
     <div className={styles.mainNav}>
       <div className={styles.codev}>
@@ -19,7 +25,11 @@ const Navbar = () => {
         </span>
       </div>
       <div className={styles.btns}>
-        <Auth />
+        {!session ? <Auth/>:(
+            <div>
+                <img src={session.user?.image || ''} className={styles.avatar} />
+            </div>
+        )}
       </div>
     </div>
   )
