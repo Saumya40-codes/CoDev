b4330b724cace1fb22b65da26447ea6b96684ce2
= <=>
2024-03-14 20:58:44 +0530

    styling

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import styles from './project.module.css'
+import Navbar from '@/app/components/MainPage/Navbar/Navbar'
 
 const project = ({ params }: { params: { id: string } }) => {
   return (
     <div className={styles.projBody}>
-
+      <div className={styles.navb}>
+      <Navbar />
+      </div>
+      <hr className={styles.ruler} />
+      <div className={styles.projContent}>
+        <h1>Project {params.id}</h1>
+      </div>
     </div>
   )
 }
