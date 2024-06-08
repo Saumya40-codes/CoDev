c009a57011258b8f9849ddf1a27e6f19b2ad94ab
= <saumyabshah90@gmail.com>
2024-06-08 10:02:37 +0530

    fix: configure server session in providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,26 @@
-import type { AppProps } from 'next/app';
+import type { Metadata } from "next";
 import { Providers } from './providers'
+import { getServerSession } from "next-auth";
+import { authConfig } from "./lib/auth/auth";
 import { Session } from "./lib/types/types";
 import "./globals.css";
 
-function RootLayout({ Component, pageProps }: AppProps) {
-  const { session } = pageProps;
+export const metadata: Metadata = {
+  title: "coDev",
+  description: "Code together",
+};
 
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+
+  const session = await getServerSession(authConfig) as Session;
+  
   return (
     <html lang="en">
       <body>
         <Providers session={session}>
-          <Component {...pageProps} />
+          {children}
         </Providers>
       </body>
     </html>
   );
 }
-
-export default RootLayout;
